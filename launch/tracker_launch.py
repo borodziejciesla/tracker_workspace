b8515c3b2cf255080c30e8d0ff3961f4e7e7f93b
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-09-20 23:42:38 +0200

    Added radar visualization node template

--- launch/tracker_launch.py
+++ launch/tracker_launch.py
@@ -1,8 +1,29 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('bag_file_createor'),
+        'config',
+        'sensor_config.yaml'
+        )
+    
     return LaunchDescription([
+        Node(
+            package='bag_file_createor',
+            namespace='bag_file_createor',
+            executable='bag_file_createor',
+            parameters = [config],
+            name='bag_file_createor'
+        ),
+        Node(
+            package='radar_preprocessor',
+            namespace='radar_preprocessor',
+            executable='radar_preprocessor_wrapper',
+            name='sim'
+        ),
         Node(
             package='tracker',
             namespace='tracker',
@@ -12,7 +33,13 @@ def generate_launch_description():
         Node(
             package='visualization',
             namespace='visualization',
+            executable='radar_visualization',
+            name='radar_visualization'
+        ),
+        Node(
+            package='visualization',
+            namespace='visualization',
             executable='tracker_visualization',
-            name='sim'
+            name='tracker_visualization'
         )
     ])
