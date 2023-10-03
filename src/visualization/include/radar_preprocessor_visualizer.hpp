4f2c1cfbefe3fa311ecebaa0c305b714674f88fe
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-10-03 23:34:42 +0200

    clean-up

--- src/visualization/include/radar_preprocessor_visualizer.hpp
+++ src/visualization/include/radar_preprocessor_visualizer.hpp
@@ -14,13 +14,7 @@ namespace visualization {
       RadarPreprocessorVisualizer(void);
 
     private:
-      // visualization_msgs::msg::MarkerArray RadarPreprocessorMessageCallback(const radar_msgs::msg::RadarScan & radar_scan_msg, const std::string & frame_id);
       void RadarPreprocessorMessageCallback(const radar_processor_msgs::msg::ScanObjects & radar_processor_scan_msg);
-      // void RadarMessageCallbackRear(const radar_msgs::msg::RadarScan & radar_scan_msg);
-      // void RadarMessageCallbackFrontLeft(const radar_msgs::msg::RadarScan & radar_scan_msg);
-      // void RadarMessageCallbackFrontRight(const radar_msgs::msg::RadarScan & radar_scan_msg);
-      // void RadarMessageCallbackRearLeft(const radar_msgs::msg::RadarScan & radar_scan_msg);
-      // void RadarMessageCallbackRearRight(const radar_msgs::msg::RadarScan & radar_scan_msg);
 
       rclcpp::Subscription<radar_processor_msgs::msg::ScanObjects>::SharedPtr radar_processor_subscribers_;
       rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr objects_marker_publishers_;
