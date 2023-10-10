60dad54aa128bdc9e0ff6ff48aa03e45dfff16b4
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2023-10-10 22:30:15 +0200

    Updated tracker package

--- src/tracker/include/tracker_wrapper.hpp
+++ src/tracker/include/tracker_wrapper.hpp
@@ -1,10 +1,15 @@
 #ifndef TRACKER_INCLUDE_TRACKER_WRAPPER_HPP_
 #define TRACKER_INCLUDE_TRACKER_WRAPPER_HPP_
 
+#include <memory>
+#include <vector>
+
 #include "rclcpp/rclcpp.hpp"
 
+#include "ghm_phd_cv_pose.hpp"
+
 #include "tracker_msgs/msg/tracker_scan.hpp"
-#include "radar_msgs/msg/radar_scan.hpp"
+#include "radar_processor_msgs/msg/scan_objects.hpp"
 
 namespace tracker_wrapper {
   class TrackerWrapper : public rclcpp::Node {
@@ -12,12 +17,15 @@ namespace tracker_wrapper {
       TrackerWrapper(void);
 
     private:
-      void RadarSubscriberCallback(const radar_msgs::msg::RadarScan & radar_msg);
+      void RadarPreprocessorSubscriberCallback(const radar_processor_msgs::msg::ScanObjects & radar_preprocessor_msg);
+      std::vector<mot::GmPhdCvPose::Measurement> ConvertPreprocessorScanToPhdInput(const radar_processor_msgs::msg::ScanObjects & radar_preprocessor_msg) const;
 
-      rclcpp::Subscription<radar_msgs::msg::RadarScan>::SharedPtr radar_subscriber_;
-
+      rclcpp::Subscription<radar_processor_msgs::msg::ScanObjects>::SharedPtr radar_subscriber_;
       rclcpp::Publisher<tracker_msgs::msg::TrackerScan>::SharedPtr tracker_publisher_;
       tracker_msgs::msg::TrackerScan tracker_scan_;
+
+      std::unique_ptr<mot::GmPhdCvPose> gm_phd_tracker_;
+      mot::GmPhdCalibrations<4u, 2u> calibrations_;
   };
 } // namespace tracker_wrapper
 
--- src/tracker/src/tracker_wrapper.cpp
+++ src/tracker/src/tracker_wrapper.cpp
@@ -1,5 +1,6 @@
 #include "tracker_wrapper.hpp"
 
+#include <algorithm>
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -13,14 +14,43 @@ using namespace std::chrono_literals;
 namespace tracker_wrapper {
   TrackerWrapper::TrackerWrapper(void) : Node("tracker_wrapper") {
     // Initialize subscriber
-    radar_subscriber_ = this->create_subscription<radar_msgs::msg::RadarScan>(
-      "/radar_preprocessor/radar_scan", 10, std::bind(&TrackerWrapper::RadarSubscriberCallback, this, _1));
+    radar_subscriber_ = this->create_subscription<radar_processor_msgs::msg::ScanObjects>(
+      "/radar_preprocessor/radar_scan", 10, std::bind(&TrackerWrapper::RadarPreprocessorSubscriberCallback, this, _1));
 
     // Initialize publisher
     tracker_publisher_ = this->create_publisher<tracker_msgs::msg::TrackerScan>("tracker_scan", 10);
+
+
+    // Initialize tracker
+    gm_phd_tracker_ = std::make_unique<mot::GmPhdCvPose>(calibrations_);
   }
 
-  void TrackerWrapper::RadarSubscriberCallback(const radar_msgs::msg::RadarScan & radar_msg) {
-    std::ignore = radar_msg;
+  void TrackerWrapper::RadarPreprocessorSubscriberCallback(const radar_processor_msgs::msg::ScanObjects & radar_preprocessor_msg) {
+    // Convert input
+    const auto measurements = ConvertPreprocessorScanToPhdInput(radar_preprocessor_msg);
+    // Run filter
+    gm_phd_tracker_->Run(0.0, measurements);
+    const auto objects = gm_phd_tracker_->GetObjects();
+  }
+
+  std::vector<mot::GmPhdCvPose::Measurement> TrackerWrapper::ConvertPreprocessorScanToPhdInput(const radar_processor_msgs::msg::ScanObjects & radar_preprocessor_msg) const {
+    std::vector<mot::GmPhdCvPose::Measurement> measurements;
+    std::transform(radar_preprocessor_msg.objects.begin(), radar_preprocessor_msg.objects.end(),
+      std::back_inserter(measurements),
+      [](const radar_processor_msgs::msg::MovingObject & preprocessor_object) {
+        mot::GmPhdCvPose::Measurement measurement;
+
+        measurement.value(0) = preprocessor_object.pose.x;
+        measurement.value(1) = preprocessor_object.pose.y;
+
+        measurement.covariance(0, 0) = preprocessor_object.pose.cov_x;
+        measurement.covariance(0, 1) = preprocessor_object.pose.cov_xy;
+        measurement.covariance(1, 0) = preprocessor_object.pose.cov_xy;
+        measurement.covariance(1, 1) = preprocessor_object.pose.cov_y;
+
+        return measurement;
+      }
+    );
+    return measurements;
   }
 } //  tracker_wrapper
